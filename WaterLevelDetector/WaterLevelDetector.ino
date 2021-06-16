433b0b8c849205c99cb0a2be24034e6e01356789
mustak <mushtaquea688@gmail.com>
2021-06-16 19:18:08 +0530

    WaterLevelDetector2

--- WaterLevelDetector/WaterLevelDetector.ino
+++ WaterLevelDetector/WaterLevelDetector.ino
@@ -38,6 +38,15 @@
 #define STAPSK  "your-password"
 #endif
 
+// defines pins numbers
+
+const int trigPin = 2;  //D4
+const int echoPin = 0;  //D3
+
+// defines variables
+long duration;
+int distance;
+
 const char *ssid = STASSID;
 const char *password = STAPSK;
 
@@ -121,6 +130,9 @@ void setup(void) {
   WiFi.begin(ssid, password);
   Serial.println("");
 
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -150,4 +162,22 @@ void setup(void) {
 void loop(void) {
   server.handleClient();
   MDNS.update();
+  // Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+delay(2000);
 }
